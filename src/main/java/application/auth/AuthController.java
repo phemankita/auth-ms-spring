c97e4f679bdbf903337a06a09e9edda0daf325e9
Hemankita Perabathini <Hemankita1@Hemankitas-MacBook-Pro-2.local>
2020-05-04 17:31:00 +0530

    Updating auth apis

---
+++ src/main/java/application/auth/AuthController.java
@@ -0,0 +1,42 @@
+package application.auth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST Controller to manage Customer database
+ *
+ */
+@RestController
+public class AuthController {
+    
+    private static Logger logger =  LoggerFactory.getLogger(AuthController.class);
+
+    /**
+     * Handle auth header
+     * @return HTTP 200 if success
+     */
+    @RequestMapping(value = "/authenticate", method = RequestMethod.GET)
+    @ResponseBody ResponseEntity<?> getAuthenticate() {
+    	logger.debug("GET /authenticate");
+    	
+    	return ResponseEntity.ok().build();
+    }
+    
+	/**
+     * Handle auth header
+     * @return HTTP 200 if success
+     */
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    @ResponseBody ResponseEntity<?> postAuthenticate() {
+    	logger.debug("POST /authenticate");
+    	
+    	return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/application/auth/LoginController.java
@@ -0,0 +1,19 @@
+package application.auth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    
+    private static Logger logger =  LoggerFactory.getLogger(LoginController.class);
+    
+    @RequestMapping("/login")
+    public String login() {
+    	logger.debug("/login");
+    	return "login";
+    }
+    
+}
